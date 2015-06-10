373e3d7a6dd8a1cf794d0063420ccf3fe6eb4e3e
DSergii <drozdsa@gmail.com>
2015-06-10 23:24:59 +0300

    add controllers

--- GymDairy/development/app/app.js
+++ GymDairy/development/app/app.js
@@ -5,30 +5,68 @@ $.material.init();
 	
 	angular
 		.module('GymDiary', ['ngRoute'])
-		.config(gymDiaryConfig);
+		.config(gymDiaryConfig)
+		.controller('MainCtrl', function($scope){
+			
+		})
+		.controller('AboutCtrl', function($scope){
+			
+		})
+		.controller('ContactCtrl', function($scope){
+			
+		})
+		.controller('ExercisesCtrl', function($scope){
+			
+		})
+		.controller('StatisticsCtrl', function($scope){
+			
+		})
+		.controller('ProfileCtrl', function($scope){
+			
+		})
+		.controller('PlayListCtrl', function($scope){
+			
+		})
+		.controller('NutritionCtrl', function($scope){
+			
+		});
 		
 	function gymDiaryConfig($routeProvider){
 		$routeProvider
+			.when('#/', {
+				templateUrl: 'index.html',
+				controller: "MainCtrl"
+			})
 			.when('/about', {
-				templateUrl: 'views/about.html'
+				templateUrl: 'views/about.html',
+				controller: "AboutCtrl"
 			})
 			.when('/contact', {
-				templateUrl: 'views/contact.html'
+				templateUrl: 'views/contact.html',
+				controller: "ContactCtrl"
 			})
 			.when('/exercises', {
-				templateUrl: 'views/exercises.html'
+				templateUrl: 'views/exercises.html',
+				controller: "ExercisesCtrl"
 			})
 			.when('/statistics', {
-				templateUrl: 'views/statistics.html'
+				templateUrl: 'views/statistics.html',
+				controller: "StatisticsCtrl"
 			})
 			.when('/profile', {
-				templateUrl: 'views/profile.html'
+				templateUrl: 'views/profile.html',
+				controller: "ProfileCtrl"
 			})
 			.when('/playlist', {
-				templateUrl: 'views/playlist.html'
+				templateUrl: 'views/playlist.html',
+				controller: "PlayListCtrl"
 			})
 			.when('/nutrition', {
-				templateUrl: 'views/nutrition.html'
+				templateUrl: 'views/nutrition.html',
+				controller: "NutritionCtrl"
+			})
+			.otherwise({
+				
 			});
 	}
 })();
